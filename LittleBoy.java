d00c2a8bf692e86c03ba427c2578311294c0cfea
ThaiCintra <thaissa.c@aluno.ifsc.edu.br>
2025-10-14 22:26:06 -0300

    Pequenos ajustes no código e melhoria na entrega

--- LittleBoy.java
+++ LittleBoy.java
@@ -41,6 +41,7 @@ public class LittleBoy extends AdvancedRobot {
 	// onScannedRobot: O que fazer ao ver outro robô
 
 	public void onScannedRobot(ScannedRobotEvent e) {
+		shoot (e);
 		setFire (1);
 		// Quando Radar encontra um inimigo
 		double angleToEnemy =
@@ -49,7 +50,34 @@ public class LittleBoy extends AdvancedRobot {
 		double extraTurn = Math.atan(36.0 / e.getDistance()) * (turnToEnemy >= 0 ? 1 : -1);
 		
 		setTurnRadarRightRadians(turnToEnemy + extraTurn);
+			
+	}
+	
+	// Configuração da Mira
+	public void shoot (ScannedRobotEvent e) {
+		double absoluteBearing = e.getBearingRadians() + getHeadingRadians();
+		double gunTurn = absoluteBearing - getGunHeadingRadians();
+		setTurnGunRightRadians(Utils.normalRelativeAngle(gunTurn));
+		// Força do Tiro
+		double firePower = decideFirePower(e);
+		setFire(firePower);
+	}
+	public double decideFirePower(ScannedRobotEvent e) {
+		double firePower = getOthers() == 1 ? 2.0 : 3.0;
 		
+		if(e.getDistance() > 400) {
+			firePower = 1.0;
+		} else if (e.getDistance() < 200) {
+			firePower = 3.0;
+		}
+		
+		if (getEnergy() <1) {
+			firePower = 0.1;
+		} else if (getEnergy() <10) {
+			firePower = 1.0;
+		}
+		
+		return Math.min(e.getEnergy() / 4, firePower);
 	}
 
 	/**
@@ -59,6 +87,7 @@ public class LittleBoy extends AdvancedRobot {
 		// Substitua a próxima linha por qualquer comportamento desejado
 		back(10);
 	}
+	
 
 	/**
 	 * onHitWall: O que fazer ao bater em uma parede
